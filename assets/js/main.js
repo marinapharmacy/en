272192d893daf0f8d013c88f4ac479bca09a476d
salsabil2022 <marinapharma@hotmail.com>
2022-11-23 08:56:00 +0300

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,8 +122,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('bx-menu')
+    this.classList.toggle('bx-x')
   })
 
   /**
@@ -147,8 +147,8 @@
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile')
         let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        navbarToggle.classList.toggle('bx-menu')
+        navbarToggle.classList.toggle('bx-x')
       }
       scrollto(this.hash)
     }
